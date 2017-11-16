ad725e86cd11650a81a42b738bba4d40901cfc23
ashishkg0022 <ashishkg0022@gmail.com>
2017-11-16 18:18:19 +0530

    removed unwanted files

--- devel/share/common-lisp/ros/gui_path/msg/_package_path_point.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package gui_path-msg)
-(cl:export '(X-VAL
-          X
-          Y-VAL
-          Y
-))
--- devel/share/common-lisp/ros/gui_path/msg/_package_point_array.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package gui_path-msg)
-(cl:export '(POINT_ARRAY-VAL
-          POINT_ARRAY
-          OBSTACLE_ARRAY-VAL
-          OBSTACLE_ARRAY
-))
